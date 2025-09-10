97cb9b1391d5a7ef7f4535bc66d5d6f707f88051
Vít Pavlík <pavlikvitek13@gmail.com>
2025-09-10 21:22:26 +0200

    generate png icon

--- generate-resources.sh
+++ generate-resources.sh
@@ -7,3 +7,8 @@ PNG=$(mktemp XXXXXXXXX.png)
 convert -background none -density 96 static/icons/key.svg -resize 64x64 $PNG
 convert $PNG static/favicon.ico
 rm $PNG
+
+PNG=$(mktemp XXXXXXXXX.png)
+
+convert -background white -density 96 static/icons/key.svg -resize 256x256 $PNG
+mv $PNG static/img/key.png
