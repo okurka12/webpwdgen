5c8d6eaccd836bb11a5b0f5c35bc6b33e90adc61
Vít Pavlík <pavlikvitek13@gmail.com>
2025-09-10 20:11:03 +0200

    clear button, bit of styling

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,5 @@
 const NUMBER_OF_BUTTONS = 8;
+const clear_button_text = "🗑️ clear"
 
 let repetitions = 1
 
@@ -29,8 +30,33 @@ function repeat(n) {
 
 function append_password_to_list(password) {
     const el = document.createElement("li")
-    el.innerHTML = `<code>${password}</code>`
+    el.innerHTML = `<code>${password}</code><span>length: ${password.length}</span>`
     document.getElementById("password-list").appendChild(el)
+    add_or_remove_clear_button()
+}
+
+function add_clear_button_if_not_already_there() {
+    const container = document.getElementById("clear-button-container")
+    if (container.childNodes.length < 1) {
+        const btn = document.createElement("button")
+        btn.setAttribute("onclick", "clear_passwords()")
+        btn.innerText = clear_button_text
+        container.appendChild(btn)
+    }
+}
+
+function add_or_remove_clear_button() {
+    const container = document.getElementById("clear-button-container")
+    if (document.getElementById("password-list").childNodes.length == 0) {
+        container.innerHTML = ""
+    } else {
+        add_clear_button_if_not_already_there()
+    }
+}
+
+function clear_passwords() {
+    document.getElementById("password-list").innerHTML = ""
+    add_or_remove_clear_button()
 }
 
 function generate() {
@@ -48,4 +74,5 @@ document.addEventListener("DOMContentLoaded", () => {
         .getElementById("fmt-string")
         .addEventListener("input", update_buttons)
     )
+    update_buttons()
 })
